922d0c18c265bd582d63ec6a0ecdcf9b73a604d4
Daniela <daniinad175.may30@gmail.com>
2024-03-25 09:57:10 -0300

    primer commit}

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload;
+let hoy = document.getElementById('hoy');
+
+let montoActual = document.getElementById('montoActual');
+
+let gastoTotal = document.getElementById('gastoTotal');
+let gastoFijo = document.getElementById('gastoFijo');
+let gastoDiario = document.getElementById('gastoDiario');
+let gastoExtra = document.getElementById('gastoExtra');
+
+let ingreso = document.getElementById('ingreso');
+
+let colchon = document.getElementById('colchon');
+
+
+function fecha() {
+    let fecha = new Date();
+    let dia = fecha.getDate();
+    let mes = fecha.getMonth() + 1;
+    let anio = fecha.getFullYear();
+    let fechaCompleta = dia + '/' + mes + '/' + anio;
+    hoy.innerHTML = "Hoy es: " + fechaCompleta;
+}
+fecha();
