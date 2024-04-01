d9358f848dedba878851b02122bc634c653016c8
Daniela <daniinad175.may30@gmail.com>
2024-04-01 20:31:25 -0300

    mostrar y ocultar opciones

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 window.onload;
 let hoy = document.getElementById('hoy');
 
-let montoActual = document.getElementById('montoActual');
-
-let gastoTotal = document.getElementById('gastoTotal');
-let gastoFijo = document.getElementById('gastoFijo');
-let gastoDiario = document.getElementById('gastoDiario');
-let gastoExtra = document.getElementById('gastoExtra');
-
-let ingreso = document.getElementById('ingreso');
-
+let ingresos = document.getElementById('ingresos');
+let gastos = document.getElementById('gastos');
 let colchon = document.getElementById('colchon');
+
 
 
 function fecha() {
@@ -22,3 +16,43 @@ function fecha() {
     hoy.innerHTML = "Dia de hoy : " + fechaCompleta;
 }
 fecha();
+
+function oculto(elem){
+    elem.style.visibility = 'hidden';
+  }
+function visible(elem){
+    elem.style.visibility = 'visible'
+   
+  switch (elem) {
+    case ingresos:
+
+         oculto(gastos);
+        oculto(colchon);
+        console.log("ingresos esta visible y se ocultan los gastos y el colchon")
+        break;
+  
+        case gastos:
+
+        oculto(ingresos);
+        oculto(colchon)
+        console.log("gastos esta visible y se ocultan los ingresos y el colchon")
+        break;
+        case colchon:
+            oculto(ingresos);
+            oculto(gastos)
+            console.log("colchon esta visible y se ocultan los ingresos y los gastos")
+            break;
+    default:
+        oculto(ingresos);
+        oculto(gastos);
+        oculto(colchon)
+  }
+  
+  
+}
+
+  
+ 
+
+
+
